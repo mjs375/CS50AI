4bd1d635b84191ad0f88e9aa49b3b1b640c6606f
mjs375 <spitzer.matthewjames@gmail.com>
2021-01-11 11:55:51 -0500

    Mini-nim game ~90%

--- 4/nim/mininim.py
+++ 4/nim/mininim.py
@@ -1,13 +1,19 @@
 from termcolor import colored
 
+import decimal
 import os
+import random
 import sys
 import time
 
 
 """
-Plays '21' version of 'Nim'.
+Plays '21' version of 'Nim'. Players take turns removing 1, 2, or 3 sticks from the pile. Person to remove the last stick loses.
 """
+
+
+
+
 
 
 class Game():
@@ -56,9 +62,36 @@ class Game():
             else:
                 if grab == "slaughter":
                     killer = self.players[self.turn]
-                    sys.exit(f"{killer} butchered their opponent. Total bloodbath. The soul weeps at such violence.\nBut they technically win the game.")
+                    sys.exit(f"{killer} butchered their opponent. Total bloodbath. The soul weeps at such (counting) violence.\n...But they technically win the game.")
+                if grab == "random" or grab.lower() == "r":
+                    move = random.randrange(1,4)
                 print("Please enter a number between 1 & 3.")
         self.dwindle(move)
+    #
+    def AI_move(self):
+        """
+        If AI is P2, it has a guaranteed win by always selecting a multiple of four, thus resulting in forcing P1 to choose the last stick.
+        If AI is P1, they will attempt to choose a multiple of four, but if unable, they will choose just 1 stick, so as to give human player as much opportunity to make an error and give them back a chance to remove sticks to give them a multiple of four again.
+        """
+        sticks = self.pile
+        #--If human player has the advantage (THEY have the multiple of four), always pick 1 to give them an opportunity to mess up and return the advantage to you:
+        if sticks == 4:
+            move = 3
+        elif sticks % 4 == 0:
+            move = 1
+        #--Calculate the next multiple of 4 after (less than) the current pile.
+        else:
+            next = sticks + (4 - sticks % 4) - 4
+            move = sticks - next
+        #
+        #--Apply the actual move:
+        self.dwindle(move)
+        print(f"The pile has {self.pile} sticks remaining. Pick up 1, 2, or 3 sticks?")
+        time.sleep(0.3)
+        name = self.players[self.turn]
+        print(f"{name} picks up {move} sticks.")
+        time.sleep(2.5)
+
     #
     def win(self):
         """
@@ -74,7 +107,7 @@ class Game():
         print(f"It's {current_player}'s turn.")
 
 
-def play(n, players):
+def play(n, players, AI):
     """
     Runs the Nim game, using OOP.
     """
@@ -84,6 +117,10 @@ def play(n, players):
     #--Assign players:
     game.players[1] = players[0]
     game.players[-1] = players[1]
+    #--AI on?
+    if AI[0] == True:
+        #--which player?
+        AI_turn = AI[1]
 
     #--Game ongoing variable:
     playing = True
@@ -93,8 +130,13 @@ def play(n, players):
         game.turner()
         #--Print the 'board'/which player is up:
         game.print()
-        #--Get current player's move:
-        game.move()
+        #--Get current player's move OR AI's move
+        if AI[0] == True and game.turn == AI_turn:
+            move = game.AI_move()
+        else:
+            game.move()
+
+
         #--Check if a winner:
         check = game.win()
         if check == True:
@@ -106,13 +148,12 @@ def play(n, players):
             playing = False
 
 
-def yes_no():
+def yes_no(question):
     ask = True
     while ask:
-        ans = input("Yes or no?\n")
+        print(question)
+        ans = input("Yes or no?\n>>> ")
         #
-        print("?", ans[0])
-        print(ans[0].lower, ans[0].lower == "y")
         if ans[0].lower() == "y":
             ask = False
             return True
@@ -124,35 +165,54 @@ def yes_no():
 
 
 
+def mechanical_typer(msg):
+    for char in msg:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        x = random.randrange(0,15)
+        x /= 100
+        time.sleep(x)
+
 
 def main():
-
-    #--Check command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python3 mininim.py [pile_size]")
+    #--PRINTING THE RULES:
+    n = 21
     #--
-    n = sys.argv[1]
-    if not n.isdigit() and n < 10 and n > 100:
-        sys.exit("Please enter a valid game size between 10 and 100.")
-    else:
-        n = int(n)
-
-    
+    msg = f"# Welcome to TWENTY-ONE, a stick-picking-up game.\n#I am a ROBOT entity in charge of ADMINISTERING the game and its rewards and/or PUNISHMENTS FOR LOSING.\n# Ahem.\n# The pile of sticks has {n} sticks to start.\n# Players take turns removing 1, 2 or 3 sticks.\n# The player who removes the last stick wins and the other player LOSES DREADFULLY AND MUST BE DEALT WITH.\n# Get stratergizing!\n"
+    mechanical_typer(msg)
+    time.sleep(1.5)
+    #--PLAYER INPUTS:
+    #--Play AI? (or human)
+    question = "Play against AI? ('no' for 2-player)"
+    AI = yes_no(question)
+    #--Player (1)'s name:
+    time.sleep(1)
     p1 = input("> Player 1's name: ")
-    p2 = input("> Player 2's name: ")
-    players = (p1, p2)
-
-
-
+    if AI == False:
+        time.sleep(1)
+        p2 = input("> Player 2's name: ")
+    else:
+        robot_names  = ["Experimental Annihilator Entity", "CODENAME ENDER", "David v3.2", "ORDINARY HUMAN", "M0THER", "NimBot", "Robbie"]
+        p2 = random.choice(robot_names)
+    #--Randomize players as P1/P2:
+    shuffle = random.choice([True, False])
+    if shuffle == True:
+        players = (p1, p2)
+        #--(AI mode is on, which player is AI):
+        AItuple = (AI, -1)
+    else:
+        players = (p2, p1)
+        AItuple = (AI, 1)
+    #--Run the game(s):
     playing = True
     while playing:
-        play(n, players)
-        #--
-        print("Would you like to play again?")
-        again = yes_no()
+        play(n, players, AItuple)
+        #--Game over, play again?
+        question = "Would you like to play again?"
+        again = yes_no(question)
         if again == False:
             playing = False
-
+    #
     sys.exit("Thanks for playing!")
 
 
